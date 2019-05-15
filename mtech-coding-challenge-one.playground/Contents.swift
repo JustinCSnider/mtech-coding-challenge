c6830fe94a749573319a7620b869c3e89158d2f0
Justin Snider <jcsnider100@gmail.com>
2019-05-15 09:35:32 -0600

    Improved challenge slightly

--- mtech-coding-challenge-one.playground/Contents.swift
+++ mtech-coding-challenge-one.playground/Contents.swift
@@ -3,18 +3,17 @@ import UIKit
 var numberArray: [Int] = []
 
 for i in 0...100 {
-    if i % 3 == 0 {
-        print("Mountainland")
-    }
-    if i % 5 == 0 {
-        print("Technical")
-    }
-    if i % 3 == 0 && i % 5 == 0 {
+    if i % 3 == 0 && i % 5 == 0 && i != 0 {
         print("Mountainland Technical College")
         numberArray.append(i)
-    }
-    if i % 3 != 0 && i % 5 != 0 {
-        print(i)
+    } else {
+        if i % 3 == 0 && i != 0 {
+            print("Mountainland")
+        } else if i % 5 == 0 && i != 0 {
+            print("Technical")
+        } else {
+            print(i)
+        }
     }
 }
 
